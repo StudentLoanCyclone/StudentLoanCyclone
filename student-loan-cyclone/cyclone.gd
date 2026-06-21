79a65eba1870381764ccc2586feb6589f6b19976
ethanEverel123 <C22351646@mytudublin.ie>
2026-06-21 01:52:13 +0100

    made a launch function to make it easier

--- student-loan-cyclone/cyclone.gd
+++ student-loan-cyclone/cyclone.gd
@@ -1,17 +1,23 @@
 extends RigidBody3D
 
 
-@export var initial_speed :float = 200.00
+@export var initial_spin :float = 200.00
 @export var spin_decay :float = 5.0
-@export var spin_max : float = 300
 
 
 var current_spin: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	current_spin = initial_speed
-	angular_velocity.y = initial_speed
+	launch()
+	
+func launch():
+	current_spin = initial_spin
+	angular_velocity.y = initial_spin
+	
+	angular_velocity.x = randf_range(0,10)
+	angular_velocity.z = randf_range(0,10)
+	
 
 
 func _physics_process(delta):
@@ -24,7 +30,7 @@ func _physics_process(delta):
 	
 	
 	#wobble when slowing
-	var upright_strength = (current_spin/spin_max) * 10
+	var upright_strength = (current_spin/initial_spin) * 10
 	var current_upright = global_transform.basis.y
 	var upright_target = Vector3.UP
 	
