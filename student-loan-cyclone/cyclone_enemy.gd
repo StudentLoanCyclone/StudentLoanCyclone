605cd125d3eb079510f881afeb4446fd4385d139
ethanEverel123 <C22351646@mytudublin.ie>
2026-06-24 17:23:21 +0100

    seperated the cyclones into AI and player scripts the AI is now functional but will need tuning

--- student-loan-cyclone/cyclone_enemy.gd
+++ student-loan-cyclone/cyclone_enemy.gd
@@ -19,21 +19,33 @@ var target : Node3D
 @export var max_stamina := 100.0
 var current_stamina = max_stamina
 
-@export var selfcamera : Camera3D
 
 @export var plastic1 : MeshInstance3D
 @export var plastic2 : MeshInstance3D
 @export var trim : MeshInstance3D
 
 
+#frame these more as states of mind than actions
+enum State {ATTACK, DEFEND, IDLE}
+var state := State.IDLE
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	if !self.is_in_group("player"):
+		randomize_colour()
 	
 	launch()
 
 
 
 func _physics_process(delta):
+	if target != null:
+		_update_state()
+	
 	#Spin decay.
 	current_spin = move_toward(current_spin, 0, spin_decay * delta)
 	
@@ -72,17 +84,11 @@ func _physics_process(delta):
 	if current_spin <= 0:
 		#queue_free()
 		pass
+		
+	match state:
+		State.ATTACK : _attack()
+		State.DEFEND : _defend()
 
-
-
-func _input(event):
-	if self.is_in_group("player") and target:
-		if event.is_action_pressed("attack"):
-			if(current_stamina >= 50):
-				attack()
-		if event.is_action_pressed("defend"):
-			if(current_stamina >= 50):
-				defend()
 
 
 
@@ -95,26 +101,43 @@ func launch():
 	angular_velocity.z = randf_range(0,10)	
 
 
-
-func attack():
-	current_stamina -= 50
+func _update_state():
 	
-	var chasedir = (target.global_position - global_position).normalized()
-	current_spin += spin_boost
-
-	linear_velocity = chasedir * 20
-
-
-
-func defend():
-	current_stamina -= 30
-
-	var distance = target.global_position - global_position
 	
-	if (distance).length() < 20:
-		#this is the same as recieve impact but eh
-		target.apply_central_impulse(distance.normalized() * 500)
-		target.current_spin -= abs(current_spin) * 0.02
+	var dist = (target.global_position - global_position).length()
+	
+	
+	if dist <= 20 and dist >= 5:
+		state = State.ATTACK
+		
+	else:
+		state = State.DEFEND
+		
+
+
+func _attack():
+	#enemy attack should also be about deciding if it can and should
+	
+	if(current_stamina >= 50):
+		current_stamina -= 50
+		
+		var chasedir = (target.global_position - global_position).normalized()
+		current_spin += spin_boost
+
+		linear_velocity = chasedir * 20
+
+
+
+func _defend():
+	if (current_stamina >= 50):
+		current_stamina -= 50
+
+		var distance = target.global_position - global_position
+		
+		if (distance).length() < 20:
+			#this is the same as recieve impact but eh
+			target.apply_central_impulse(distance.normalized() * 500)
+			target.current_spin -= abs(current_spin) * 0.02
 
 
 
@@ -147,16 +170,48 @@ func recieve_impact(force: Vector3, opposing_spin: float):
 
 
 
+func get_stamina():
+	return current_stamina
+
+
+
+func randomize_colour():
+	print("randomizing colour....")
+	
+	# These colour combinations,a nd their reverses, are what the enemy will be randomised with.
+	var colour_combos: Array[Array] = [
+	[Color("#ff7d0c"), Color("#e6bb62")], # Orange and Yellow
+	[Color("#ef002b"), Color("#b98837")], # Red and Gold
+	[Color("#a933ff"), Color("#00cf38")], # Green and Purple
+	[Color("#0085da"), Color("#d8c300")], # Blue and Yellow
+	[Color("#000000"), Color("#ffffff")], # Black and White
+	[Color("#8601cd"), Color("#ff4fa9")]  # Pink and Purple
+	]
+	
+	var colour_num = randi()%6
+	var colour_combo = colour_combos[colour_num]
+	
+	var random_order = randi()%2
+	
+	if random_order == 0:
+		print("order 1...")
+		print(plastic1.get_surface_override_material(0))
+		plastic2.get_surface_override_material(0).albedo_color = colour_combo[0]
+		trim.get_surface_override_material(0).albedo_color = colour_combo[1]
+		
+	else:
+		print("order 2...")
+		plastic1.get_surface_override_material(0).albedo_color = colour_combo[1]
+		plastic2.get_surface_override_material(0).albedo_color = colour_combo[1]
+		trim.get_surface_override_material(0).albedo_color = colour_combo[0]
+	
+	
 func get_target():
 	return target
 
 
 
-func get_stamina():
-	return current_stamina
 
-
-	
 	
 	
 	
